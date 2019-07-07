35b7527baf454083c9f343517c632f1f8012d918
Andres <andresefr24@gmail.com>
2019-07-07 23:40:02 +0200

    Updates

---
+++ precache-manifest.de7d5368054730f633ebe49d70c27e31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31485b5b56f29d060e24a4d2d58138f",
+    "url": "/andresferrero/index.html"
+  },
+  {
+    "revision": "4f47fb2c6d7ee4ee18eb",
+    "url": "/andresferrero/static/css/2.c36eb9f9.chunk.css"
+  },
+  {
+    "revision": "32a232a0066db1558491",
+    "url": "/andresferrero/static/css/main.c4a67534.chunk.css"
+  },
+  {
+    "revision": "4f47fb2c6d7ee4ee18eb",
+    "url": "/andresferrero/static/js/2.46cb06ed.chunk.js"
+  },
+  {
+    "revision": "32a232a0066db1558491",
+    "url": "/andresferrero/static/js/main.37adff82.chunk.js"
+  },
+  {
+    "revision": "3e30824b2ede1ba15d4c",
+    "url": "/andresferrero/static/js/runtime~main.d5febd96.js"
+  }
+]);
